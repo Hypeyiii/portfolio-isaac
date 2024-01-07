b77f83cf0843b1cc894b839c749f5e4ab38b47d6
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-01-07 05:57:09 -0600

    add:changes

--- language.js
+++ language.js
@@ -26,6 +26,14 @@ const translations = {
         'time-3':'Agosto, 2021',
         'doing-3':'Ingreso al Estudio Superior',
         'doing-description-3':'Ingreso a la Faculdad de Ingeniería Mecácnica y Eléctrica (FIME) en la carrera de Ingeniero Administrador de Sistemas (IAS).',
+        'tech-description':'A lo largo de mi tiempo como',
+        'web-dev':'Desarrollador Web',
+        'tech-description-2':', he tenido la oportunidad de desarrollar habilidades con diferentes tipos de',
+        'game-description':'Proyecto principal al cursar la materia "Programación Orientada a Objetos". Hecho completamente en el lenguaje Java',
+        'tesla-description':'Proyecto personal para la creación de la página web de Tesla. Hecho con HTML y Tailwind. (Sin funcionalidad).',
+        'footer':'© 2024 Isaac Frias. Derechos Reservados',
+        'about':'Sobre mí',
+        'contact':'Contacto',
     },
     'en': {
         'title': 'Hey, I´m Isaac',
@@ -53,6 +61,14 @@ const translations = {
         'time-3':'August, 2021',
         'doing-3':'Entering University',
         'doing-description-3':'Entering the Faculty of Mechanical and Electrical Engineering (FIME) in the Systems Administrator Engineer (IAS) career.',
+        'tech-description':'Throughout my time as a',
+        'web-dev':'Web Developer',
+        'tech-description-2':'I have had the opportunity to develop skills with different types of',
+        'game-description':'Main project while taking the "Object-Oriented" Programming course. Completely made in the Java language.',
+        'tesla-description':'Personal project for the creation of the Tesla website. Made with HTML and Tailwind. (Non-functional).',
+        'footer':'© 2024 Isaac Frias. All Rights Reserved',
+        'about':'About me',
+        'contact':'Contact',
     }
 };
 
