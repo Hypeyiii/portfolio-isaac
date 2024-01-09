2f25778f674992488d4befe98d760f3b3d5e8e3b
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-01-08 19:50:36 -0600

    add:changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["index.html","node_modules/flowbite/dist/flowbite.min.js"],
+  content: ["index.html","./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
